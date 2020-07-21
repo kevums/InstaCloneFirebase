74e7d042c9d868bc524ba7f7a83a6ed8c94d0f7e
Kevin Landry <kevinjlandry@gmail.com>
2020-07-21 18:02:16 -0500

    Initial Commit

---
+++ InstaCloneFirebase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstaCloneFirebase
+//
+//  Created by Kevin Landry on 7/21/20.
+//  Copyright © 2020 Kevin Landry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
