8c4c474635fcb6b9aab3f85b55e317ab43570550
Kevin Landry <kevinjlandry@gmail.com>
2020-07-28 17:44:26 -0500

    first commit

---
+++ InstaCloneFirebase/FeedCell.swift
@@ -0,0 +1,24 @@
+//
+//  FeedCell.swift
+//  InstaCloneFirebase
+//
+//  Created by Kevin Landry on 7/28/20.
+//  Copyright © 2020 Kevin Landry. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
